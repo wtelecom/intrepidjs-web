c83d981e7606367b57446850815bb34d3273f1df
lammoth <chelestra2003@yahoo.es>
2014-11-20 16:58:57 +0100

    Changes ¬¬

--- modules/documentation/public/javascripts/controllers/index_controller.js
+++ modules/documentation/public/javascripts/controllers/index_controller.js
@@ -14,22 +14,25 @@ angular.module('IntrepidJS').controller('DocController',
     [
         '$scope',
         '$state',
+        '$timeout',
         '$location',
+        'restService',
         '$anchorScroll',
         'i18n',
-        function ($scope, $state, $location, $anchorScroll, i18n){
+        function ($scope, $state, $timeout, $location, restService, $anchorScroll, i18n){
             $anchorScroll.yOffset = 75;
             var lang = i18n.userLanguage;
             if (lang != 'es' && lang != 'en') {
                 lang = 'es';
             }
-            $.getJSON( "documentation/public/resources/doc_" + lang + ".json", function( data ) {
-              $scope.api = data;
-              });
-            $scope.goTo = function(id) {
-                $location.hash(id);
-            };
-            $anchorScroll();
+            restService.get({}, "documentation/public/resources/doc_" + "es" + ".json", function(data) {
+                $scope.api = data;
+                    $scope.goTo = function(id) {
+                        $location.hash(id);
+                    };
+                    $anchorScroll();
+            },
+            function(){});
         }
     ]
 );
